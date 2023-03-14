d8b094d81369309a4bccba167e3a3311fe6c5fe4
isla23 <79399514+isla23@users.noreply.github.com>
2023-03-14 23:32:13 +0800

    后勤

---
+++ autoLSupport.py
@@ -0,0 +1,311 @@
+#=============================================#
+#                                             #
+#                 导入所需模块                 #
+#                                             #
+#=============================================#
+
+import logging
+import cv2
+import time
+import random
+import datetime
+import win32api
+import win32gui
+import win32con
+from os import path
+import numpy as np
+from PIL import ImageGrab
+from skimage.metrics import structural_similarity
+
+#=============================================#
+#                                             #
+#                 定义所需常量                 #
+#                                             #
+#=============================================#
+
+
+
+#=================截图比对区域=================#
+IMAGE_PATH = 'initial_IMG/'#读取截图的路径
+MAIN_MENU_IMAGE_BOX = [0.63,0.52,0.75,0.58]#主界面判断区域
+#[0.65,0.58,0.75,0.63]
+#[0.63,0.52,0.75,0.58]                       
+L_SUPPORT_IMAGE_BOX = [0.1, 0.13, 0.25, 0.19]  # 后勤完成界面判断区域
+DESKTOP_IMAGE_BOX = [0.10,0.20,0.22,0.35]#模拟器桌面判断区域         
+
+#=================点击拖动区域=================#
+
+#收后勤支援
+L_SUPPORT_STEP1_CLICK_BOX = [0.53,0.51,0.60,0.54]#确认后勤完成
+L_SUPPORT_STEP2_CLICK_BOX = [0.53,0.61,0.62,0.64]#再次派出
+
+#启动游戏
+START_GAME_STEP1_CLICK_BOX = [0.14,0.23,0.18,0.28]#点击图标启动
+START_GAME_STEP2_CLICK_BOX = [0.50,0.70,0.50,0.70]#点击一次
+START_GAME_STEP3_CLICK_BOX = [0.50,0.75,0.50,0.75]#点击开始 
+
+#关闭游戏
+CLOSE_GAME_CLICK_BOX = [0.56,0.02,0.57,0.04]
+
+#=============================================#
+#                                             #
+#                 基本功能函数                 #
+#                                             #
+#=============================================#
+
+#一个好程序都应该有一个较为优雅的启动提醒界面？
+def preface():    
+    for x in range(3,-1,-1):
+        mystr =">>> "+str(x)+"s 后将开始操作，请切换至模拟器界面"
+        print(mystr,end="")
+        print("\b" * (len(mystr)*2),end = "",flush=True)
+        time.sleep(1)
+    logger.debug("开始操作")
+
+
+#随机等待一段时间,控制在minTime~maxTime之间
+def wait(minTime,maxTime):
+    waitTime = minTime + (maxTime - minTime) * random.random()
+    time.sleep(waitTime)
+
+
+#获取模拟器窗口数据
+def getWindowData():
+    windowName = "少女前线 - MuMu模拟器"
+    windowNameDesktop = "MuMu模拟器"
+    hwnd = win32gui.FindWindow(None,windowName)#根据窗口名称找到窗口句柄
+    hwnd_desktop = win32gui.FindWindow(None,windowNameDesktop)
+    if hwnd == 0 and hwnd_desktop == 0:
+        logger.debug("未找到窗口界面,程序自动退出！")
+        exit(0)
+    elif hwnd != 0:
+        left,top,right,bottom = win32gui.GetWindowRect(hwnd)#获取窗口的位置数据
+    elif hwnd_desktop != 0:
+        left,top,right,bottom = win32gui.GetWindowRect(hwnd_desktop)#获取窗口的位置数据
+    width  = right - left
+    height = bottom - top
+    return [left,top,right,bottom,width,height]
+
+
+#获取指定区域box的截图
+def getImage(box):
+    #windowData = [left,top,right,bottom,width,height]        
+    windowData = getWindowData()
+    imgLeft   = windowData[0] + int(windowData[4] * box[0])
+    imgTop    = windowData[1] + int(windowData[5] * box[1])
+    imgRight  = windowData[0] + int(windowData[4] * box[2])
+    imgBottom = windowData[1] + int(windowData[5] * box[3])
+    img = ImageGrab.grab((imgLeft,imgTop,imgRight,imgBottom))
+    return img
+    
+    
+#点击box内随机一点，如果提供具体xy偏量，则点击精确的点
+def mouseClick(box,minTime,maxTime,exact_x = 0,exact_y = 0):
+    #box = [left,top,right,bottom]
+    windowData = getWindowData()
+    width  = box[2] - box[0]
+    height = box[3] - box[1]
+    if exact_x == 0 and exact_y == 0:
+        clickX = windowData[0] + (int)(windowData[4] * box[0] + windowData[4] * width  * random.random())
+        clickY = windowData[1] + (int)(windowData[5] * box[1] + windowData[5] * height * random.random())
+    else:
+        clickX = windowData[0] + (int)(windowData[4] * box[0]) + exact_x
+        clickY = windowData[1] + (int)(windowData[5] * box[1]) + exact_y
+    clickPos = (clickX,clickY)
+    win32api.SetCursorPos(clickPos)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0,0,0)
+    wait(minTime,maxTime)
+
+
+#模拟鼠标拖动，box为起始区域,times为拖动次数,distance为单次拖动距离
+#dx,dy为组成移动方向向量，frame_interval为鼠标拖动帧间隔,越小鼠标拖动越快
+#multi_interval为连续拖动时的时间间隔
+def mouseDrag(box,dx,dy,times,distance,frame_interval,multi_interval):
+    windowData = getWindowData()
+    width  = box[2] - box[0]
+    height = box[3] - box[1]
+    for i in range(times):
+        dragX = windowData[0] + int(windowData[4] * box[0] + windowData[4] * width  * random.random())
+        dragY = windowData[1] + int(windowData[5] * box[1] + windowData[5] * height * random.random())
+        dragPos = (dragX, dragY)
+        win32api.SetCursorPos(dragPos)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
+        for i in range(distance):
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE,dx,dy,0,0)
+            time.sleep(frame_interval)
+        time.sleep(0.2)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0,0,0)
+        time.sleep(multi_interval)
+
+
+#模拟Ctrl和滚轮实现缩放地图
+#direct = 0 : 放大      direct = 1 : 缩小   times为连续缩放次数
+def scaleMap(box,direct,times):
+    windowData = getWindowData()
+    width  = box[2] - box[0]
+    height = box[3] - box[1]
+    scaleX = windowData[0] + int(windowData[4] * box[0] + windowData[4] * width  * random.random())
+    scaleY = windowData[1] + int(windowData[5] * box[1] + windowData[5] * height * random.random())
+    scalePos = (scaleX, scaleY)
+    win32api.SetCursorPos(scalePos)
+    win32api.keybd_event(0x11,0,0,0)#按下Ctrl键
+    for i in range(times):
+        if direct == 0:
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,1)
+        else:
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,-1)
+        wait(0.5,0.7)
+    win32api.keybd_event(0x11, 0, win32con.KEYEVENTF_KEYUP,0)    
+    time.sleep(1)
+        
+
+#比较两图片吻合度，结构相似性比较法（真的好用）
+def imageCompare(img1,img2):
+    gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    (score, diff) = structural_similarity(gray_img1, gray_img2, full=True)
+    return score > 0.95
+
+
+#显示窗口
+def showWindow():
+    windowName = "少女前线 - MuMu模拟器"
+    windowNameDesktop = "MuMu模拟器"
+    hwnd = win32gui.FindWindow(None,windowName)#根据窗口名称找到窗口句柄
+    hwnd_desktop = win32gui.FindWindow(None,windowNameDesktop)
+    if hwnd == 0 and hwnd_desktop == 0:
+        logger.debug("未找到窗口界面,程序自动退出！")
+        exit(0)
+    elif hwnd != 0:
+        logger.debug("显示少女前线 - MuMu模拟器！")
+        win32gui.ShowWindow(hwnd,win32con.SW_SHOW)
+    elif hwnd_desktop != 0:
+        logger.debug("显示MuMu模拟器！")
+        win32gui.ShowWindow(hwnd_desktop,win32con.SW_SHOW)
+    time.sleep(3)
+
+
+#隐藏窗口
+def hideWindow():
+    windowName = "少女前线 - MuMu模拟器"
+    windowNameDesktop = "MuMu模拟器"
+    hwnd = win32gui.FindWindow(None,windowName)#根据窗口名称找到窗口句柄
+    hwnd_desktop = win32gui.FindWindow(None,windowNameDesktop)
+    if hwnd == 0 and hwnd_desktop == 0:
+        logger.debug("未找到窗口界面,程序自动退出！")
+        exit(0)
+    elif hwnd != 0:
+        win32gui.ShowWindow(hwnd,win32con.SW_HIDE)
+    elif hwnd_desktop != 0:
+        win32gui.ShowWindow(hwnd_desktop,win32con.SW_HIDE)
+    time.sleep(3)
+#=============================================#
+#                                             #
+#                 高级功能函数                 #
+#                                             #
+#=============================================#
+    
+#判断是否是主界面
+def isMainMenu():
+    initImage = cv2.imread(IMAGE_PATH+"main_menu.png")
+    capImage  = getImage(MAIN_MENU_IMAGE_BOX)
+    capImage  = cv2.cvtColor(np.asarray(capImage),cv2.COLOR_RGB2BGR)
+    return imageCompare(initImage,capImage)
+    
+#判断是否是委托完成界面
+def isLSupport():
+    initImage = cv2.imread(IMAGE_PATH+"L_support.png")
+    capImage  = getImage(L_SUPPORT_IMAGE_BOX)
+    capImage  = cv2.cvtColor(np.asarray(capImage),cv2.COLOR_RGB2BGR)
+    return imageCompare(initImage,capImage)
+
+#判断是否是模拟器桌面
+def isDesktop():
+    initImage = cv2.imread(IMAGE_PATH+"desktop.png")
+    capImage  = getImage(DESKTOP_IMAGE_BOX)
+    capImage  = cv2.cvtColor(np.asarray(capImage),cv2.COLOR_RGB2BGR)
+    return imageCompare(initImage,capImage)
+
+#收后勤支援
+def takeLSupport():
+    logger.debug("ACTION: 收派后勤")
+    mouseClick(L_SUPPORT_STEP1_CLICK_BOX,1,1)
+    mouseClick(L_SUPPORT_STEP2_CLICK_BOX,1,1)
+
+#启动游戏
+def startGame():
+    logger.debug("ACTION: 启动游戏")
+    mouseClick(START_GAME_STEP1_CLICK_BOX,30,30)
+    mouseClick(START_GAME_STEP2_CLICK_BOX,30,30)
+    mouseClick(START_GAME_STEP3_CLICK_BOX,30,30)
+
+
+#关闭游戏
+def closeGame():
+    logger.debug("ACTION: 关闭游戏")
+    mouseClick(CLOSE_GAME_CLICK_BOX,5,5)
+
+#=============================================#
+#                                             #
+#                 本程序主函数                 #
+#                                             #
+#=============================================#
+
+# 创建Logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# 创建Handler
+# 终端Handler
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)  
+
+if __name__ == "__main__": 
+    # 文件Handler
+    currentPath = path.dirname(__file__)
+    fileHandler = logging.FileHandler(currentPath+'/log.log', mode='w', encoding='UTF-8')
+    fileHandler.setLevel(logging.NOTSET)
+    # Formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    consoleHandler.setFormatter(formatter)
+    fileHandler.setFormatter(formatter)  
+    # 添加到Logger中
+    logger.addHandler(consoleHandler)
+    logger.addHandler(fileHandler)
+    
+    preface()
+    L_SupportCount = 0
+    failCount = 0
+    while True:
+        showWindow()
+        if isMainMenu():
+            logger.debug("主菜单")
+            failCount = 0
+        elif isLSupport():
+            logger.debug("委托完成")
+            failCount = 0
+            takeLSupport()
+            L_SupportCount += 1
+            logger.debug("收派后勤数："+str(L_SupportCount))
+            continue
+        elif isDesktop():
+            logger.debug("模拟器桌面")
+            failCount = 0
+            startGame()
+            continue
+        else:#有的时候卡了或者是怎么样了，直接关了重启
+            logger.debug("ERROR： 当前状态未知!")
+            failCount += 1
+            if failCount == 4:
+                mouseClick([0.3,0.45,0.4,0.55],2,3)
+            if failCount >= 5:
+                img = getImage([0,0,1,1])
+                img.save("errorRecord/"+str(failCount)+".png")
+                logger.debug("无法确定当前状态,关闭重启！")
+                closeGame()
+            else:
+                time.sleep(5)
+            continue
+        hideWindow()#隐藏窗口，这样你就可以同时做其他事了
+        wait(200,240)
